42152c210c98e8033062cc421bf9ea7cefdda457
najibbaraka <najib.baraka@epitech.eu>
2024-09-11 22:02:57 +0200

    verification de OS user pour definir le DB_PWD

--- admin/config/config.php
+++ admin/config/config.php
@@ -9,6 +9,11 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'gurdil');
 define('DB_CHARSET', 'utf8mb4');
 define('DB_USER', 'root');
-define('DB_PWD', '');
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+if (strpos($user_agent, 'Win') !== false) {
+    define('DB_PWD', '');
+} elseif (strpos($user_agent, 'Mac') !== false) {
+    define('DB_PWD', 'root');
+}
 
 define('DSN', DB_ENGINE .':host='. DB_HOST .';dbname='. DB_NAME .';charset='. DB_CHARSET );
